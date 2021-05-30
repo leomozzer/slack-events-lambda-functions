2c55bade67178ebcb4a86f3ea314aa04792930b6
leomozzer <mozzer.ferraz@gmail.com>
2021-05-30 13:34:57 -0300

    fixed urlencoded

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ 'extended': false }))
 app.use((req, res, next) => {
     next()
 })
